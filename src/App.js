703af2b27d7e04ec2c9c6c344bc374e9244efa0e
Siva Dinesh .k <ksd.ccbp4@gmail.com>
2024-05-26 00:43:29 +0530

    Navigation added for sections

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import React from 'react'
+import './App.css'
+import Navbar from './components/navbar'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <section>
+        <Navbar />
+      </section>
+      <section id="HomePage">HomePage</section>
+      <section id="About">Portfolio</section>
+      <section id="Skills">Skills</section>
+      <section id="Projects">projects</section>
+      <section id="Contact">contact</section>
+    </div>
   )
 }
 
