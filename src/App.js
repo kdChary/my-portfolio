7a73f41eeb51d65ac056e5ce6a06a37638e127fd
Siva Dinesh .k <ksd.ccbp4@gmail.com>
2024-05-26 01:26:08 +0530

    modifications made

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
 import React from 'react'
 import './App.css'
 import Navbar from './components/navbar'
+import Hero from './components/Hero';
 
 const App = () => {
   return (
-    <div>
-      <section>
-        <Navbar />
-      </section>
-      <section id="HomePage">HomePage</section>
-      <section id="About">Portfolio</section>
-      <section id="Skills">Skills</section>
-      <section id="Projects">projects</section>
-      <section id="Contact">contact</section>
-    </div>
-  )
+		<div>
+			<section id="HomePage">
+				<Navbar />
+				<Hero />
+			</section>
+			<section id="About">Portfolio</section>
+			<section id="Skills">Skills</section>
+			<section id="Projects">projects</section>
+			<section id="Contact">contact</section>
+		</div>
+	);
 }
 
 export default App
---
+++ src/components/Hero/index.js
@@ -0,0 +1,20 @@
+import './index.css';
+
+import React from 'react';
+
+const Hero = () => {
+	return (
+		<div className="hero">
+			<div className="wrapper">
+				<div className="img-container">
+					<img
+						src="https://images.pexels.com/photos/697509/pexels-photo-697509.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+						alt="profile"
+					/>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Hero;
