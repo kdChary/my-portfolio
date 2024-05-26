99c422788e34938454b800efc32534b77d03e465
Siva Dinesh .k <ksd.ccbp4@gmail.com>
2024-05-26 09:25:29 +0530

    text & animations added

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -1,16 +1,82 @@
+import React from 'react';
+import { motion } from 'framer-motion';
+
 import './index.css';
 
-import React from 'react';
+const textVariants = {
+	initial: {
+		x: -500,
+		opacity: 0,
+	},
+	animate: {
+		x: 0,
+		opacity: 1,
+		transition: {
+			duration: 1,
+			staggerChildren: 0.1,
+		},
+	},
+	scrollButton: {
+		opacity: 0,
+		y: 10,
+		transition: {
+			duration: 2,
+			repeat: Infinity,
+		},
+	},
+};
+const sliderVariant = {
+	initial: {
+		x: 0,
+	},
+	animate: {
+		x: '-190%',
+		transition: {
+			repeat: Infinity,
+			repeatType: 'mirror',
+			duration: 20,
+		},
+	},
+};
 
 const Hero = () => {
 	return (
 		<div className="hero">
 			<div className="wrapper">
-				<div className="img-container">
-					<img
-						src="https://images.pexels.com/photos/697509/pexels-photo-697509.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-						alt="profile"
+				<motion.div
+					className="text-container"
+					variants={textVariants}
+					initial="initial"
+					animate="animate">
+					<motion.h2 variants={textVariants}>Siva Dinesh</motion.h2>
+					<motion.h1 variants={textVariants}>
+						Full-Stack Web Developer
+					</motion.h1>
+					<motion.div className="hero-btns" variants={textVariants}>
+						<motion.button className="my-projects-btn" variants={textVariants}>
+							See my Works
+						</motion.button>
+						<motion.button className="my-contact-btn" variants={textVariants}>
+							Contact Me
+						</motion.button>
+					</motion.div>
+					<motion.img
+						src="images/scroll.png"
+						alt="scroll"
+						className="scroll-img"
+						variants={textVariants}
+						animate="scrollButton"
 					/>
+				</motion.div>
+				<motion.div
+					className="sliding-text"
+					variants={sliderVariant}
+					initial="initial"
+					animate="animate">
+					Developer Creator Influencer
+				</motion.div>
+				<div className="image-container">
+					<img src="images/hero.png" alt="profile" />
 				</div>
 			</div>
 		</div>
